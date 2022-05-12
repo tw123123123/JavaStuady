b8a58616038f7def1ac44c949cbd7e96199152e9
27861 <2786105454@qqc.com>
2022-05-12 14:41:38 +0800

    demo-eureka-provider2 v1.0 版本到本地仓库

---
+++ demo-eureka-provider2/src/main/java/com/qdc/demoeurekaprovider2/controller/PortController.java
@@ -0,0 +1,18 @@
+package com.qdc.demoeurekaprovider2.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PortController {
+
+    @Value("${server.port}")
+    String port;
+    //测试端口号
+
+    @RequestMapping(value = "/port")
+    public String testport(){
+        return "hello,I am from"+port;
+    }
+}
